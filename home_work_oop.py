7094e9e8457dc046d5a741598298e0083218a64b
Vladimir Rybalchenko <tazik.ne@gmail.com>
2021-05-25 17:35:29 +0300

    added README.md

--- home_work_oop.py
+++ home_work_oop.py
@@ -28,11 +28,6 @@ class Lecturer(Mentor):
     pass
 
 class Reviewer(Mentor):
-    def __init__(self, name, surname):
-        self.name = name
-        self.surname = surname
-        self.courses_attached = []
-
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
